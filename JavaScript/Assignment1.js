d262d1be35f025c0ed58f28961d13879a03b45de
Yashi <yashi.khandelwal27@gmail.com>
2021-12-08 11:45:48 +0530

    Assignment1

---
+++ JavaScript/Assignment1.js
@@ -0,0 +1,14 @@
+let Name = "Yashi"
+let Email = "yashi.khandelwal27@gmail.com"
+let age = 20
+let address = "Indore"
+let phoneNo = 6266080110
+let gender = "female"
+let martialStatus = "false"
+
+console.log("name:"+Name);
+console.log("email:"+Email);
+console.log("Age:"+age);
+console.log("Address:"+address);
+console.log("Gender:"+gender);
+console.log("MartialStatus:"+martialStatus);
