384d1dd9f97a2095101f62f81bacecc310592f5d
Yashi Khandelwal <65656933+yashi27-27@users.noreply.github.com>
2021-12-09 08:25:09 +0530

    Update Assignment9.js

--- JavaScript/Assignment9.js
+++ JavaScript/Assignment9.js
@@ -7,7 +7,7 @@ console.log(employeesData);
 
 //2. Add a function to return list of employees that contains "am" in his name. Add it in separate file and import it to call in another file.*/
 import { Name } from './Names';
-const employeesNameUpdate = searchName(employeesData);
+const employeesNameUpdate = Name(employeesData);
 console.log(employeesNameUpdate);
 
 //3.3. Take another paramter as salary and keep it above 500000 in numbers in json and print it upto one decimal place only with unit as Lac
