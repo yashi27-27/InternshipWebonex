7bf433d152062e4147e22863dd1cb8235e0e1afc
Yashi Khandelwal <65656933+yashi27-27@users.noreply.github.com>
2021-12-09 08:10:37 +0530

    Update Assignment8.js

--- JavaScript/Assignment8.js
+++ JavaScript/Assignment8.js
@@ -1,43 +1,63 @@
 //1. Create a function that takes a number and prints all even numbers upto that number
 
-let evenNumbers =(n) =>{
+const getevenNumbers =(n) =>{
     for(let i=0;i<=n;i++){
         if(i%2==0)
             console.log(i);
     }
 }
-evenNumbers(10);
+getevenNumbers(10);
 
 //2. Create a function that takes 2 parameter => an array of employees and age and returns all employees whose age is less than input age.
-const emparr = [
+const empolyeeArray = [
     {
     name: 'Yashi',
-    age: 20
+    age: 20,
+    email:"abc@gmail.com",
+    address:"Indore",
+    gender:"female",
+    martialStatus:false
 },
 {
     name: 'Chhaya',
-    age: 40
+    age: 40,
+    email:"bcd@gmail.com",
+    address:"Bhopal",
+    gender:"female",
+    martialStatus:false
 },
 {
     name: 'Samir',
-    age: 45
+    age: 45,
+    email:"efg@gmail.com",
+    address:"India",
+    gender:"male",
+    martialStatus:false
 },
 {
     name: 'Purabi',
-    age: 50
+    age: 50,
+    email:"hab@gmail.com",
+    address:"Ujjain",
+    gender:"female",
+    martialStatus:true
 },
 {
     name: 'Ananya',
-    age: 60
+    age: 60,
+    email:"c00@gmail.com",
+    address:"Indore",
+    gender:"female",
+    martialStatus:true
 }
 ]
-function emp (emparr,ageNumber){
-    return emparr.filter(emparr => emparr.age < ageNumber);
+function emp (empArray,ageNumber){
+    return emparr.filter(empArray => emparr.age < ageNumber);
 }
 console.log(emp(emparr,40));
 
 //3.3. Create a function that takes 2 parameter => an array of employees and an attribute and returns all employees sorted based on the emoplees attribute being passed as second parameter.
-function sortEmployees(emparr, attribute){ 
+function sortEmployees(empArr, attribute){ 
     emparr.sort( function(first, second){  
                                 if(first[attribute] > second[attribute])
                                     return 1;
@@ -47,6 +67,11 @@ function sortEmployees(emparr, attribute){
                                 })
 }
 
-sortEmployees(emparr, 'name');
-console.log(emparr);
+sortEmployees(empArr, 'name');
+console.log(empArr);
+sortEmployees(empArr, 'age');
+console.log(empArr);
+sortEmployees(empArr, 'email');
+console.log(empArr);
+
 
