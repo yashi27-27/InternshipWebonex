3f56aadeb63e05a8e5fa006507b10a05025031cb
Yashi Khandelwal <65656933+yashi27-27@users.noreply.github.com>
2021-12-20 14:42:53 +0530

    Update Assignment9.js

--- JavaScript/Assignment9.js
+++ JavaScript/Assignment9.js
@@ -6,12 +6,12 @@ for(const index in employeesData)
 console.log(employeesData);
 
 //2. Add a function to return list of employees that contains "am" in his name. Add it in separate file and import it to call in another file.*/
-import { Name } from './Names';
+import { filterEmployeesName } from './Names';
 const employeesNameUpdate = Name(employeesData);
 console.log(employeesNameUpdate);
 
 //3.3. Take another paramter as salary and keep it above 500000 in numbers in json and print it upto one decimal place only with unit as Lac
-import employeesData from './empList.jsonmpList'
+import employeesData from './empList.jsonList'
 for(const index in employeesData){
     employeesData[index].salary= ((500000*(Math.random()+1))/100000).toFixed(1) + 'Lac';
 }
